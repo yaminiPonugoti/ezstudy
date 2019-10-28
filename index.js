da999bf8138790679ed277d7d8940ad7c9c0f61f
yaminiPonugoti <yponugot@purdue.edu>
2019-10-28 19:54:55 -0400

    tried to get welcome page working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js
+// // index.js
 
 const path = require("path");
 const express = require("express");
@@ -6,8 +6,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || "8000";
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.get("/", (req, res) => {
-  res.status(200).send("ezstudy!!");
+  res.render("index", { title: "Home" });
 });
 
 app.listen(port, () => {
